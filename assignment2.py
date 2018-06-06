7ff536c30e8064c088791ca1498d1eea02b60a28
sonalisharma990 <sonalisharmasonali20@gmail.com>
2018-06-06 22:38:19 +0530

    assignment2

---
+++ assignment2.py
@@ -0,0 +1,26 @@
+#Q1 print anything
+print("I am sonali")
+
+#Q2 join two string
+print("acad"+"view")
+
+#Q3 print values of x,y,z
+x=2
+y=3
+z=4
+print("%d %d %d"%(x,y,z))
+
+#Q4 print let's get started
+print("let\'s get started")
+
+#Q5
+s="acadview"
+course="python"
+fee=5000
+print("hi you are in %s,your course is %s and fee is %d"%(s,course,fee))
+
+#Q6
+name="tony stark"
+salary=1000000
+print("%s %d"%(name,salary))
+
