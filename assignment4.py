bdece41d6164ccb26e5d38863dbf085407830443
lovishagumber <gumberlovisha7@gmail.com>
2018-06-08 14:31:57 +0530

    assignment

---
+++ assignment4.py
@@ -0,0 +1,32 @@
+#Q1
+t=(1,2,3,4,5)
+b=len (t)
+print(b)
+
+#Q2
+t=(4,6,7,3,9)
+print(max(t))
+print(min(t))
+
+#Q3
+t=(3,5,2,1)
+print(3*5*2*1)
+
+#Q4
+s=set((3,2,4,5))
+print(s)
+
+#Q5
+a=input("enter name")
+b=input("enter age")
+c=input("enter height")
+d={'name':a,
+   'age':b,
+   'height':c}
+print(d)
+
+#Q6not done
+
+#Q7
+a=("MISSISSIPPI")
+print(a.count('S'))
