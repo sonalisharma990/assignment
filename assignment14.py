5f64eb16b17f501bd2a70081a1909346499e64f9
sonalisharma990 <39939139+sonalisharma990@users.noreply.github.com>
2018-06-26 21:35:55 +0530

    Update assignment14.py

--- assignment14.py
+++ assignment14.py
@@ -5,13 +5,14 @@ j.reverse()
 n=int(input("enter the no. of line you want"))
 for i in range(0,n):
     print(j[i])
+f.close()
 
 
 
 
 #Q2
 a="dance"
-f=open("file6.txt","r")
+f=open("file6.txt",'r')
 b=f.read()
 c=b.split()
 print(b.count(a))
@@ -32,7 +33,7 @@ f.close()
 i=0
 f=open("file1.txt",encoding="utf8")
 x=(f.readlines())
-f1=open("file4.txt","r")
+f1=open("file4.txt",encoding="utf8")
 y=(f1.readlines())
 f3=open("file5.txt","w")
 for i,j in zip(x,y):
@@ -40,5 +41,28 @@ for i,j in zip(x,y):
 f.close()
 f1.close()
 f3.close()
+
+
+#Q5
+import random
+list=[]
+sortedlist=[]
+for i in range(0,10):
+    list.append(random.randint(1,10))
+f=open("random.txt","w")
+for s in list:
+    r="".join(str(s))
+    f.write(r)
+f.close()
+f1=open("random.txt","r")
+t=f1.read()
+for u in t:
+    if(u.isdigit()):
+        v="".join(u)
+        sortedlist.append(v)
+sortedlist.sort()
+w=open("sorted.txt","w")
+w.write(str(sortedlist))
+f.close()
 
 
