b67104bf235ff74dbf1a9597ad68eda4394127eb
sonalisharma990 <39939139+sonalisharma990@users.noreply.github.com>
2018-07-04 18:55:27 +0530

    Update assignment18.py

--- assignment18.py
+++ assignment18.py
@@ -1,79 +1,55 @@
 #Q1
+my_dict = {'name':'Sonali Sharma', 'phno':"9800000089"}
 from tkinter import *
+from tkinter.filedialog import askopenfile
 from tkinter import messagebox
-d={'Sonali':9800000089,'Aman':9000898989,'Aditi':2020000001,'Aina':9001010001,'Lovisha':9877800000}
-root=Tk()
-lb1=Label(root,text="Phone Directory")
-lb1.pack()
-scrl=Scrollbar(root)
-scrl.pack(side=RIGHT,fill=Y)
-myList=Listbox(root,yscrollcommand=scrl.set)
-i=1
-for line in d:
-    myList.insert(i,line+"-"+str(d[line]))
-    i=i+1
-myList.pack(side=LEFT,fill=BOTH)
-scrl.config(command=myList.yview)
-lbname=Label(root,text="Enter Name")
-lbphn=Label(root,text="Enter phone No.")
-e1=Entry(root)
-e2=Entry(root)
-btn=Button(root,text="Save")
+def cmd():
 
-lbname.pack()
-e1.pack()
-lbphn.pack()
-e2.pack()
-btn.pack()
-mainloop()
-
-
-
-
+    root = Tk()
+    root.title("*NEW DETAILS*")
+    root.geometry("350x200")
+    name_label = Label(root, text="NEW NAME")
+    name_label.pack()
+    name_text_box =(Entry(root, bd=1))
+    name_text_box.pack()
+    ph_label = Label(root, text="NEW PHONE NUMBER")
+    ph_label.pack()
+    ph_text_box =(Entry(root, bd=1))
+    ph_text_box.pack()
+    def add_new():
+        name=name_text_box.get()
+        print(name)
+        phn=ph_text_box.get()
+        print(phn)
+        my_dict = {'name': name, 'phno':phn}
+        label1 = Label(root, text="You have entered the information to the dictionary")
+        label1.pack()
+    enter_button = Button(root, text="Enter", command=add_new)
+    enter_button.pack()
+    root.mainloop()
+print("\n")
+print(my_dict['name'])
+print(my_dict.get('phno'))
 
 #Q2
-fL = {}
-from tkinter import *
-from tkinter import messagebox
-d={'Sonali':9800000089,'Aman':9000898989,'Aditi':2020000001,'Aina':9001010001,'Lovisha':9877800000}
-def savephnn():
-    if (e1.index(END)==0 or e2.index(END)==0):
-        messagebox.showwarning("Warning","Please enter all values")
-    else:
-        nam = e1.get()
-        phn = int(e2.get())
-        myList.insert(i, nam + "-" + str(phn))
-
-        e1.delete(0, END)
-        e2.delete(0, END)
-        messagebox.showinfo("Congrats","Contact addded")
-root=Tk()
-lb1=Label(root,text="Phone Directory")
-lb1.pack()
-scrl=Scrollbar(root)
-scrl.pack(side=RIGHT,fill=Y)
-myList=Listbox(root,yscrollcommand=scrl.set)
-i=1
-for line in d:
-    myList.insert(i,line+"-"+str(d[line]))
-    i=i+1
-myList.pack(side=LEFT,fill=BOTH)
-scrl.config(command=myList.yview)
-lblname=Label(root,text="Enter Name")
-lblphn=Label(root,text="Enter phone No.")
-e1=Entry(root)
-e2=Entry(root)
-btn=Button(root,text="Save",command=savephnn)
-
-lblname.pack()
-e1.pack()
-lblphn.pack()
-e2.pack()
-btn.pack()
+master = Tk()
+master.title("GUI")
+master.geometry("500x500")
+scrollbar = Scrollbar(master)
+scrollbar.pack( side = RIGHT, fill = Y )
+mylist = Listbox(master, yscrollcommand = scrollbar.set)
+mylist.insert(END, "NAME:"+ my_dict['name'])
+mylist.insert(END, "PHNNO: " + str(my_dict['phno']))
+mylist.pack( side = LEFT, fill = BOTH )
+scrollbar.config( command = mylist.yview )
+lb=Label(master,text="GUI")
+lb.pack()
+import tkinter as tk
+frame = tk.Frame(master)
+frame.pack()
+slogan = tk.Button(frame,text="Enter New Details",command=cmd)
+slogan.pack(side=tk.LEFT)
+button = tk.Button(frame,text="Exit",command=exit)
+button.pack(side=tk.LEFT)
 mainloop()
-
-
-
-
-
 
